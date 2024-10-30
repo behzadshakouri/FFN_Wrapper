deb5093baf38f8125d5d55fd0461ef9432af19de
Behzad Shakouri <behzad.shakouri92@gmail.com>
2024-10-30 10:37:33 -0400

    10.30

--- ffnwrapper.cpp
+++ ffnwrapper.cpp
@@ -34,20 +34,16 @@ bool FFNWrapper::DataProcess()
     // Load the whole data (OpenHydroQual output).
     CTimeSeriesSet<double> InputTimeSeries("/home/behzad/Projects/FFNWrapper/output.txt",true);
 
-    vector<int> inputcolumns;
-    vector<int> outputcolumns;
+
     //columns.push_back(1); // A: //t It's not reading this column!!!
     inputcolumns.push_back(0); // Input 1: D(4): Settling element (1)_Coagulant:external_mass_flow_timeseries
     inputcolumns.push_back(98); // Input 2: CV(100): Reactor (1)_Solids:inflow_concentration
     outputcolumns.push_back(20); // Output: V(22): Settling element (1)_Solids:concentration
 
-    arma::mat InputData = InputTimeSeries.ToArmaMat(inputcolumns);
-    arma::mat OutputData = InputTimeSeries.ToArmaMat(outputcolumns);
+    //arma::mat InputData = InputTimeSeries.ToArmaMat(inputcolumns);
+    //arma::mat OutputData = InputTimeSeries.ToArmaMat(outputcolumns);
 
-    FFNWrapper F;
-    F.data = &InputTimeSeries;
-    F.inputcolumns = inputcolumns;
-    F.outputcolumns = outputcolumns;
+    data = &InputTimeSeries;
 
     return true;
 }
@@ -61,7 +57,7 @@ bool FFNWrapper::Train()
 
     // Initialize the network
     FFN<> model;
-    model.Add<Linear>(8); // Input Single Layer
+    model.Add<Linear>(4); // Input Single Layer
     model.Add<Sigmoid>(); // Base Layer
     model.Add<Linear>(3); // Hidden Single Layer
     model.Add<LogSoftMax>(); // Loss Function Layer
--- main.cpp
+++ main.cpp
@@ -10,27 +10,13 @@ using namespace std;
 int main()
 {
 
+
     //Data.save("Data.csv", arma::file_type::raw_ascii);
 
-    // Load the whole data (OpenHydroQual output).
-    CTimeSeriesSet<double> InputTimeSeries("/home/behzad/Projects/FFNWrapper/output.txt",true);
-
-    vector<int> inputcolumns;
-    vector<int> outputcolumns;
-    //columns.push_back(1); // A: //t It's not reading this column!!!
-    inputcolumns.push_back(0); // Input 1: D(4): Settling element (1)_Coagulant:external_mass_flow_timeseries
-    inputcolumns.push_back(98); // Input 2: CV(100): Reactor (1)_Solids:inflow_concentration
-    outputcolumns.push_back(20); // Output: V(22): Settling element (1)_Solids:concentration
-
-    arma::mat InputData = InputTimeSeries.ToArmaMat(inputcolumns);
-    arma::mat OutputData = InputTimeSeries.ToArmaMat(outputcolumns);
-
     FFNWrapper F;
-    F.data = &InputTimeSeries;
-    F.inputcolumns = inputcolumns;
-    F.outputcolumns = outputcolumns;
-
+    F.DataProcess();
     F.Train();
+
     //F.Test();
 
     // Split the labels from the training set and testing set respectively.
