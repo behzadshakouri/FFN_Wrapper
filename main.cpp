b17484bc1b834a13a39d844f9a75e6832ecb0a1e
Behzad Shakouri <behzad.shakouri92@gmail.com>
2024-11-12 16:58:43 -0500

    11.12 Loop RSI Final

--- main.cpp
+++ main.cpp
@@ -46,6 +46,59 @@ int main()
     // Defining Output(s)
     mymodelstruct.outputcolumns.push_back(2); // Output: V(11): Settling element (1)_Solids:concentration
 
+    //Model creator
+    ModelCreator modelCreator;
+    modelCreator.lag_frequency = 3;
+    modelCreator.maximum_superficial_lag = 5;
+    modelCreator.total_number_of_columns = 2;
+    modelCreator.max_number_of_layers = 2;
+    modelCreator.max_lag_multiplier = 10;
+
+    QFile results(QString::fromStdString(path) + "modelresults.txt");
+    QTextStream out;
+    if (results.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        out.setDevice(&results);
+    } else {
+        // Handle file open error
+        qDebug() << "Error opening file!";
+        return 0;
+    }
+
+    for (int i=0; i<100; i++)
+
+    {
+
+        modelCreator.CreateRandomModelStructure(&mymodelstruct);
+
+        //CModelStructure mymodelstruct2;
+        //CModelStructure mymodelstruct3(mymodelstruct);
+        //modelCreator.CreateRandomModelStructure(&mymodelstruct2);
+
+        //qDebug()<<"Model2 ?= Model1"<<(mymodelstruct2==mymodelstruct);
+        //qDebug()<<"Model3 ?= Model1"<<(mymodelstruct3==mymodelstruct);
+
+        // Running FFNWrapper
+        if (mymodelstruct.ValidLags())
+        {   FFNWrapper F;
+            F.ModelStructure = mymodelstruct;
+            F.Initiate();
+            F.Training();
+            F.Testing();
+            F.PerformanceMetrics();
+
+            qDebug()<< "i = " << i << ", " << mymodelstruct.ParametersToString() << ", nMSE = " << F.nMSE << ", R2 = " << F._R2;
+            out << "i = " << i << ", " << mymodelstruct.ParametersToString() << ", nMSE = " << F.nMSE << ", R2 = " << F._R2 << "\n";
+
+            F.DataSave();
+
+            //data::Save("model.xml","model", F);
+        }
+        else
+            i--;
+    }
+
+    results.close();
+
     /*
 
     FFNWrapper F;
@@ -95,59 +148,6 @@ int main()
     F2.DataSave();
 
     */
-
-    //Model creator
-    ModelCreator modelCreator;
-    modelCreator.lag_frequency = 3;
-    modelCreator.maximum_superficial_lag = 5;
-    modelCreator.total_number_of_columns = 2;
-    modelCreator.max_number_of_layers = 2;
-    modelCreator.max_lag_multiplier = 10;
-
-    QFile results(QString::fromStdString(path) + "modelresults.txt");
-    QTextStream out;
-    if (results.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        out.setDevice(&results);
-    } else {
-       // Handle file open error
-       qDebug() << "Error opening file!";
-       return 0;
-    }
-
-    for (int i=0; i<100; i++)
-
-    {
-
-    modelCreator.CreateRandomModelStructure(&mymodelstruct);
-
-    CModelStructure mymodelstruct2;
-    CModelStructure mymodelstruct3(mymodelstruct);
-    modelCreator.CreateRandomModelStructure(&mymodelstruct2);
-
-    //qDebug()<<"Model2 ?= Model1"<<(mymodelstruct2==mymodelstruct);
-    //qDebug()<<"Model3 ?= Model1"<<(mymodelstruct3==mymodelstruct);
-
-    // Running FFNWrapper
-    if (mymodelstruct.ValidLags())
-    {   FFNWrapper F;
-        F.ModelStructure = mymodelstruct;
-        F.Initiate();
-        F.Training();
-        F.Testing();
-        F.PerformanceMetrics();
-
-        qDebug()<< "i = " << i << ", " << mymodelstruct.ParametersToString() << ", nMSE = " << F.nMSE << ", R2 = " << F._R2;
-        out << "i = " << i << ", " << mymodelstruct.ParametersToString() << ", nMSE = " << F.nMSE << ", R2 = " << F._R2 << "\n";
-
-        F.DataSave();
-
-        //data::Save("model.xml","model", F);
-    }
-    else
-        i--;
-    }
-
-    results.close();
 
     return 0;
 }
